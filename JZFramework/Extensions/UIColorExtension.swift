0910f37ce9e1c20171f0acfbbf1531e13d96cac9
Jeff Zhang <jeffz@esuperfund.com.au>
2018-03-09 17:05:20 +1100

    First commit

---
+++ JZFramework/Extensions/UIColorExtension.swift
@@ -0,0 +1,31 @@
+//
+//  UIColorExtension.swift
+//  JZFramework
+//
+//  Created by Jeff Zhang on 9/3/18.
+//  Copyright © 2018 Jeff Zhang. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+    
+    //Get UIColor by RGB
+    public convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: a
+        )
+    }
+    //Get UIColor by HEX
+    public convenience init(rgb: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF,
+            a: a
+        )
+    }
+}
